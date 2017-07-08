baadb0ba3f5ca1086f510bb234142167963d4c89
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2017-07-08 10:08:32 +1000

    Tooling update

---
+++ test/Serilog.Filters.Expressions.Tests/PackagingTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Xunit;
+
+namespace Serilog.Filters.Expressions.Tests
+{
+    public class PackagingTests
+    {
+        [Fact]
+        public void AssemblyVersionIsSet()
+        {
+            var version = typeof(LoggerFilterConfigurationExtensions).GetTypeInfo().Assembly.GetName().Version;
+            Assert.Equal("1", version.ToString(1));
+        }
+    }
+}
